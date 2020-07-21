fb777637f9ee2512cfd7dd614e94b599e307d4a3
VanamaSravya <60335751+VanamaSravya@users.noreply.github.com>
2020-07-21 17:17:03 +0530

    Commit

---
+++ Newyear/src/main/java/Chocolates.java
@@ -0,0 +1,8 @@
+
+package maven.demo;
+
+public class Chocolates {
+	String a="Sweets";
+	String b="chocloates";
+
+}
---
+++ Newyear/src/main/java/maven/demo/chocolates.java
@@ -0,0 +1,7 @@
+package maven.demo;
+
+public class chocolates {
+	String a="Sweets";
+	String b="chocloates";
+
+}
